7cdc16165840063081c71e71b1ca799ced622e17
dbrosy <dbrosy@hotmail.com>
2017-05-13 00:44:57 +1000

    Update app.js

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,8 +3,14 @@ var path = require('path');
 var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
-var app = express();
 var port = 8000;
+var app = express();
+
+
+// on the request to root
+app.get('/', function (req, res) {
+    res.send('<b>My</b> first express http server running on port: ' + port);
+});
 
 app.listen(port);
 console.log('Server is running on port: ' + port);
